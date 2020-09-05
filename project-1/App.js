420c3f9981145345eb6f11caeb75a0d564ad5190
vishwa608 <61463107+vishwa608@users.noreply.github.com>
2020-09-05 09:41:29 +0530

    Add files via upload

---
+++ project-1/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+
+export class RedButton extends Component{
+  render(){
+    return(
+      <Button color="red" title="Click Me"></Button>
+    )
+  }
+}
+
+export default class App extends Component  {
+  render(){
+    return(
+      <View style={{marginTop: 500, width: "50%"}}>
+      <RedButton/>
+       <Text>My first React Component</Text>
+      </View>
+    );
+  }
+}
